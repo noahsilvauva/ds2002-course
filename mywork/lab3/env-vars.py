11d16f0080abe56099dc79d8a49b386e27502533
noahsilvauva <acv7qc@virginia.edu>
2024-02-18 17:13:17 -0500

    Adding an unfinished version of the env-vars.py file. Still need to figure out python3 path issue.

---
+++ mywork/lab3/env-vars.py
@@ -0,0 +1,19 @@
+#!/WindowsApps/python3
+
+import os
+
+# Set the environment variables
+os.environ["FIRST_NAME"] = input('What is your first name? ')
+os.environ["MIDDLE_NAME"] = input('What is your middle name? ')
+os.environ["LAST_NAME"] = input('What is your last name? ')
+
+# Fetch the environment variables
+FIRST_NAME_ENV = os.getenv("FIRST_NAME")
+MIDDLE_NAME_ENV = os.getenv("MIDDLE_NAME")
+LAST_NAME_ENV = os.getenv("LAST_NAME")
+
+# Print the environment variables
+print(FIRST_NAME_ENV)
+print(MIDDLE_NAME_ENV)
+print(LAST_NAME_ENV)
+
