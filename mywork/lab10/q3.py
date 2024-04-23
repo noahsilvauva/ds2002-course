e3639b08013283901faa4d3177c10f2bb98d4c8d
Noah Silva <acv7qc@virginia.edu>
2024-04-23 04:10:56 +0000

    Part 3 of Lab 10

---
+++ mywork/lab10/q3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import json
+
+# Invalid JSON data
+data = '{"invalid_json_key": "value"}'
+
+try:
+  # Attempt to load the JSON data
+  json.loads(data)
+except json.JSONDecodeError as e:
+  # Print the JSON import error
+  print(f"JSON import error: {e}")
